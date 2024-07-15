110ccaed5cb1535f575ef621785edaad9f77ea47
Cheng Vue <csalvue@gmail.com>
2024-07-14 22:15:10 -0500

    Add NavigationBar component

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavigationBar() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Cheng Vue</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+
+    
+  );
+}
+
+export default NavigationBar;
