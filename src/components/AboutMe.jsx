bb5ecf80cb5293bab47c1045fcea64f20b4490cb
Cheng Vue <csalvue@gmail.com>
2024-07-17 19:16:17 -0500

    add css folder and add css to about-me component

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,8 @@
+import '../css/aboutMe.css'
+
 function AboutMe() {
   return (
-    <div className='about-me'>
+    <div className='about-me container'>
       <p>Full-stack web developer with a passion for building fast, efficient, and user-friendly web application. 
         Thrive on team-building and excited about contributing to innovative technology solutions and empowering users.</p>
     </div>
