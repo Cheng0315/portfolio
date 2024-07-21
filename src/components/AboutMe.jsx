e28a63d2c2c8e8a77bcc8acc11a6f7f58454c32d
Cheng Vue <csalvue@gmail.com>
2024-07-21 17:58:14 -0500

    add contact info to aboutMe component

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,6 +9,13 @@ function AboutMe() {
         I enjoy turning complex problems into simple, beautiful, and easy to read code.
         When not coding, you'll find me cooking, playing the guitar, or jogging in the park.
       </p>
+      <p>Interested in working together? We should queue up a chat. I'll buy the coffee.</p>
+      <div className='contact'>
+        <h1>Contact</h1>
+        <p className='phone'> Phone: (559) 286-6435</p>
+        <p className='email'> Email: csalvue@gmail.com</p>
+        <p className='location'> Location: Minneapolis - Saint Paul</p>
+      </div>
     </div>
   );
 }
