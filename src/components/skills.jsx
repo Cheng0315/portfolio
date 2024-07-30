33300acafaa1b91f6f6f3ecf32245c9cbef46d04
Cheng Vue <csalvue@gmail.com>
2024-07-29 21:11:58 -0500

    add logos and css to skills component

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,24 +1,99 @@
+import '../css/skills.css'
+
 function Skills() {
   
   return (
-    <div className='container'>
-      <h1>Technical Skills</h1>
-      <h6>JAVASCRIPT</h6>
-      <h6>TYPESCRIPT</h6>
-      <h6>Python</h6>
-      <h6>JAVA</h6>
-      <h6>NODEJS</h6>
-      <h6>EXPRESSJS</h6>
-      <h6>REACT</h6>
-      <h6>HTML5</h6>
-      <h6>CSS3</h6>
-      <h6>JQUERY</h6>
-      <h6>BOOTSTRAP</h6>
-      <h6>TAILWIND</h6>
-      <h6>POSTGRESQL</h6>
-      <h6>GIT</h6>
-      <h6>GITHUB</h6>
-    </div>
+    <section id='technical-skills' className='technical-skills'>
+      <div className='container'>
+        <h1>Technical Skills</h1>
+        <div className='dev-logos-container'>
+          <div className="dev-logos">
+            <a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript'>
+              <img src='https://cdn.svgporn.com/logos/javascript.svg' alt='js'/>
+              <h6>JAVASCRIPT</h6>
+            </a>
+          </div>
+          <div className="dev-logos">
+            <a href='https://www.typescriptlang.org/'>
+              <img src='https://cdn.svgporn.com/logos/typescript.svg' alt='js'/>
+              <h6>TYPESCRIPT</h6>
+            </a>
+          </div>
+          <div className="dev-logos">
+            <a href='https://www.python.org/'>
+              <img src='https://cdn.svgporn.com/logos/python.svg' alt='js'/>
+              <h6>PYTHON</h6>
+            </a>
+          </div>
+          <div className="dev-logos">
+            <a href='https://www.java.com/en/'>
+              <img src='https://cdn.svgporn.com/logos/java.svg' alt='js'/>
+              <h6>JAVA</h6>
+            </a>
+          </div>
+          <div className="dev-logos">
+            <a href='https://nodejs.org/en'>
+              <img src='https://cdn.svgporn.com/logos/nodejs.svg' alt='js'/>
+              <h6>NODEJS</h6>
+            </a>
+          </div>
+          <div className="dev-logos">
+            <a href='https://expressjs.com/'>
+              <img src='https://cdn.svgporn.com/logos/express.svg' alt='js'/>
+              <h6>EXPRESS</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://reactjs.org/'>
+              <img src='https://cdn.svgporn.com/logos/react.svg' alt='react'/>
+              <h6>REACT</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5'>
+              <img src='https://cdn.svgporn.com/logos/html-5.svg' alt='html5'/>
+              <h6>HTML5</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3'>
+              <img src='https://cdn.svgporn.com/logos/css-3.svg' alt='css3'/>
+              <h6>CSS3</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://jquery.com/'>
+              <img src='https://cdn.svgporn.com/logos/jquery.svg' alt='jquery'/>
+              <h6>JQUERY</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://tailwindcss.com/'>
+              <img src='https://cdn.svgporn.com/logos/tailwindcss.svg' alt='bootstrap'/>
+              <h6>TAILWIND</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://www.postgresql.org/'>
+              <img src='https://cdn.svgporn.com/logos/postgresql.svg' alt='pg'/>
+              <h6>POSTGRESQL</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://git-scm.com/' target='_blank' rel="noopener noreferrer">
+              <img src='https://cdn.svgporn.com/logos/git-icon.svg' alt='git'/>
+              <h6>GIT</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://github.com/' target='_blank' rel="noopener noreferrer">
+              <img src='https://cdn.svgporn.com/logos/github-icon.svg' alt='github'/>
+              <h6>GITHUB</h6>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
