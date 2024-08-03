505cf0911fea04ae731baf6ed2c39eac3a53e6d7
Cheng Vue <csalvue@gmail.com>
2024-08-02 22:12:34 -0500

    add css to navigation bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,15 +3,21 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCodeBranch } from '@fortawesome/free-solid-svg-icons'
+import '../css/navigationBar.css'
 
 function NavigationBar() {
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <a href="/"><FontAwesomeIcon icon={faCodeBranch} size='2x'/></a>
-          <Navbar.Brand href="#home">Cheng Vue</Navbar.Brand>
-          <Nav className="me-auto">
+          <div className="navbar-brand">
+            <a href="/"><FontAwesomeIcon icon={faCodeBranch} size="3x"/></a>
+            <a href="/" className="info">
+              <div className="name">CHENG VUE</div>
+              <div className="title">FULL STACK WEB DEVELOPER</div>
+            </a>
+          </div>
+          <Nav className="ml-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
             <Nav.Link href="#skills">Skills</Nav.Link>
