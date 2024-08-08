00464844344341ffdaae6fa73e8ee9212170f50a
Cheng Vue <csalvue@gmail.com>
2024-08-08 18:55:59 -0500

    update skills component

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -8,75 +8,99 @@ function Skills() {
         <h1>Technical Skills</h1>
         <div className='dev-logos-container'>
           <div className="dev-logos">
-            <a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript'>
+            <a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript' rel="noopener noreferrer">
               <img src='https://cdn.svgporn.com/logos/javascript.svg' alt='js'/>
               <h6>JAVASCRIPT</h6>
             </a>
           </div>
-          <div className="dev-logos">
+          <div className="dev-logos" rel="noopener noreferrer" >
             <a href='https://www.typescriptlang.org/'>
-              <img src='https://cdn.svgporn.com/logos/typescript.svg' alt='js'/>
+              <img src='https://cdn.svgporn.com/logos/typescript.svg' alt='typescript'/>
               <h6>TYPESCRIPT</h6>
             </a>
           </div>
-          <div className="dev-logos">
+          <div className="dev-logos" rel="noopener noreferrer">
             <a href='https://www.python.org/'>
-              <img src='https://cdn.svgporn.com/logos/python.svg' alt='js'/>
+              <img src='https://cdn.svgporn.com/logos/python.svg' alt='python'/>
               <h6>PYTHON</h6>
             </a>
           </div>
-          <div className="dev-logos">
+          <div className="dev-logos" rel="noopener noreferrer">
             <a href='https://www.java.com/en/'>
-              <img src='https://cdn.svgporn.com/logos/java.svg' alt='js'/>
+              <img src='https://cdn.svgporn.com/logos/java.svg' alt='java'/>
               <h6>JAVA</h6>
             </a>
           </div>
-          <div className="dev-logos">
+          <div className='dev-logos' rel="noopener noreferrer">
+            <a href='https://spring.io/projects/spring-boot' target='_blank' rel="noopener noreferrer">
+              <img src='https://cdn.svgporn.com/logos/spring.svg' alt='springBoot'/>
+              <h6>SPRING BOOT</h6>
+            </a>
+          </div>
+          <div className="dev-logos" rel="noopener noreferrer">
             <a href='https://nodejs.org/en'>
-              <img src='https://cdn.svgporn.com/logos/nodejs.svg' alt='js'/>
+              <img src='https://cdn.svgporn.com/logos/nodejs.svg' alt='nodejs'/>
               <h6>NODEJS</h6>
             </a>
           </div>
-          <div className="dev-logos">
+          <div className="dev-logos" rel="noopener noreferrer">
             <a href='https://expressjs.com/'>
-              <img src='https://cdn.svgporn.com/logos/express.svg' alt='js'/>
+              <img src='https://cdn.svgporn.com/logos/express.svg' alt='express'/>
               <h6>EXPRESS</h6>
             </a>
           </div>
-          <div className='dev-logos'>
+          <div className='dev-logos' rel="noopener noreferrer">
             <a href='https://reactjs.org/'>
               <img src='https://cdn.svgporn.com/logos/react.svg' alt='react'/>
               <h6>REACT</h6>
             </a>
           </div>
-          <div className='dev-logos'>
+          <div className='dev-logos' rel="noopener noreferrer">
+            <a href='https://redux.js.org/'>
+              <img src='https://cdn.svgporn.com/logos/redux.svg' alt='redux'/>
+              <h6>REDUX</h6>
+            </a>
+          </div>
+          <div className='dev-logos' rel="noopener noreferrer">
             <a href='https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5'>
               <img src='https://cdn.svgporn.com/logos/html-5.svg' alt='html5'/>
               <h6>HTML5</h6>
             </a>
           </div>
-          <div className='dev-logos'>
+          <div className='dev-logos' rel="noopener noreferrer">
             <a href='https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3'>
               <img src='https://cdn.svgporn.com/logos/css-3.svg' alt='css3'/>
               <h6>CSS3</h6>
             </a>
           </div>
           <div className='dev-logos'>
-            <a href='https://jquery.com/'>
+            <a href='https://jquery.com/' rel="noopener noreferrer">
               <img src='https://cdn.svgporn.com/logos/jquery.svg' alt='jquery'/>
               <h6>JQUERY</h6>
             </a>
           </div>
           <div className='dev-logos'>
-            <a href='https://tailwindcss.com/'>
-              <img src='https://cdn.svgporn.com/logos/tailwindcss.svg' alt='bootstrap'/>
+            <a href='https://tailwindcss.com/' rel="noopener noreferrer">
+              <img src='https://cdn.svgporn.com/logos/tailwindcss.svg' alt='tailwind'/>
               <h6>TAILWIND</h6>
             </a>
           </div>
           <div className='dev-logos'>
-            <a href='https://www.postgresql.org/'>
+            <a href='https://getbootstrap.com/' rel="noopener noreferrer">
+              <img src='https://cdn.svgporn.com/logos/bootstrap.svg' alt='bootstrap'/>
+              <h6>BOOTSTRAP</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://www.postgresql.org/' rel="noopener noreferrer">
               <img src='https://cdn.svgporn.com/logos/postgresql.svg' alt='pg'/>
               <h6>POSTGRESQL</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://www.mysql.com/' target='_blank' rel="noopener noreferrer">
+              <img src='https://cdn.svgporn.com/logos/mysql.svg' alt='mysql'/>
+              <h6>MSQL</h6>
             </a>
           </div>
           <div className='dev-logos'>
@@ -89,6 +113,18 @@ function Skills() {
             <a href='https://github.com/' target='_blank' rel="noopener noreferrer">
               <img src='https://cdn.svgporn.com/logos/github-icon.svg' alt='github'/>
               <h6>GITHUB</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://angular.dev/' target='_blank' rel="noopener noreferrer">
+              <img src='https://cdn.svgporn.com/logos/angular.svg' alt='angular'/>
+              <h6>ANGULAR</h6>
+            </a>
+          </div>
+          <div className='dev-logos'>
+            <a href='https://www.mongodb.com/' target='_blank' rel="noopener noreferrer">
+              <img src='https://cdn.svgporn.com/logos/mongodb.svg' alt='mongodb'/>
+              <h6>MONGODB</h6>
             </a>
           </div>
         </div>
